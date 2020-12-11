0f9168e2bfa706ed0d1d9a0b85c10b22b7126bd9
NEPUGE <roman_sukharev_7409@mail.ru>
2020-12-12 01:42:04 +0500

    done

---
+++ Homework/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.output2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 37);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Массив #1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите размер массива:";
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.output.Location = new System.Drawing.Point(15, 104);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(174, 69);
+            this.output.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Массив #2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(237, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(250, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введите рамер двумерного массива:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(240, 61);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 37);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(355, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сгенерировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(397, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // output2
+            // 
+            this.output2.Location = new System.Drawing.Point(240, 104);
+            this.output2.Multiline = true;
+            this.output2.Name = "output2";
+            this.output2.ReadOnly = true;
+            this.output2.Size = new System.Drawing.Size(247, 69);
+            this.output2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(333, 61);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 37);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 348);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.output2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox output2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Homework/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework
+{
+    public partial class Form1 : Form 
+    {
+        float[] array; 
+        int razmer;
+        readonly string mydocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        FileStream file = null;
+
+        float[,] array2;
+        int n;
+        int l;
+        private char[] tempString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            razmer = int.Parse(textBox1.Text);  
+            array = new float[razmer]; 
+            Random rnd = new Random(); 
+            for (int i = 0; i < razmer; i++) 
+            {
+                array[i] = rnd.Next(0, 100); 
+                output.Text += array[i].ToString() + " "; 
+            }
+
+        }      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            output.Text = null; 
+            array = null; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            n = int.Parse(textBox2.Text);
+            l = int.Parse(textBox3.Text);
+            array2 = new float[n,l];
+            Random rnd2 = new Random();
+
+            file = new FileStream(mydocs + @"\Array2.txt", FileMode.OpenOrCreate);
+
+
+            for (int i = 0; i < n; ++i)
+            {
+                string str;
+                str = "";
+                Process.Start(mydocs + @"\Array2.txt");
+                for (int j = 0; j < l; ++j)
+                {
+                    array2[i, j] = rnd2.Next(0, 100);
+                    str += array2[i, j].ToString() + " " ;
+                }
+                str += "\r\n";
+                output2.Text += str;
+                byte[] tempBytes = Encoding.Default.GetBytes(str);
+                file.Write(tempBytes, 0, tempBytes.Length);
+            }
+            if (file != null)
+                file.Close();
+            
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            output2.Text = null;
+            array2 = null;
+           
+          
+            file = new FileStream(mydocs + @"\Array2.txt", FileMode.OpenOrCreate);
+            file.SetLength(0);
+            array2 = null;
+            Process.Start(mydocs + @"\Array2.txt");
+            if (file != null)
+            file.Close();
+           
+        }
+
+    }
+}
